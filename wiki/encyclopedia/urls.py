aab27abd6afdf4254af4f0365404ff02d69295d5
rakinplaban <rakinshahriar54@gmail.com>
2021-02-11 11:56:26 +0600

    uncommented edit's path

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("newpg/",views.newpg, name="newpg"),
-   # path("wiki/<str:entry>/edit",views.newpg, name = "editentry"),
+    path("wiki/<str:entry>/edit",views.edit, name = "editentry"),
     path("wiki/<str:entry>", views.entry, name="wiki_page"),
     path("pagenotfound/",views.entry,name="pagenotfound"),
     path("search/",views.search,name="search")
